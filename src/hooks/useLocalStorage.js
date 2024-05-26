a8ec7c90ca310d9bb373d5927203f966edf77a00
Tormod <tormod.flesjo@gmail.com>
2024-05-26 16:07:38 +0200

    quick commit

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export default function useLocalStorage(key, initialValue) {
+export function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(initialValue);
 
   const [firstLoadDone, setFirstLoadDone] = useState(false);
