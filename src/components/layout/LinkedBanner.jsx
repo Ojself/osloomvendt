f2fe9c4d8b5242cb65500488142bf8d1026475a7
Tormod <tormod.flesjo@gmail.com>
2024-05-29 10:54:56 +0200

    quick commit

--- src/components/layout/LinkedBanner.jsx
+++ src/components/layout/LinkedBanner.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const LinkedBanner = ({ href = '/' }) => {
   return (
     <Link href={href}>
-      <h2 className='cursor-pointer select-none bg-whitish px-2 text-center font-anton text-3xl text-blackish sm:text-4xl md:px-4 md:text-5xl lg:px-6 lg:text-6xl'>
+      <h2 className='cursor-pointer select-none bg-whitish px-2 text-center font-anton text-2xl text-blackish sm:text-4xl md:px-4 md:text-5xl lg:px-6 lg:text-6xl'>
         OSLO OMVENDT
       </h2>
     </Link>
