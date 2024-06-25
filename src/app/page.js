43c41eebf42956b656429edca01e832ff4dd9b30
Tormod <tormod.flesjo@gmail.com>
2024-06-25 09:37:21 +0200

    quick commit

--- src/app/page.js
+++ src/app/page.js
@@ -14,15 +14,6 @@ export default function Home() {
   return (
     <main className='h-[93vh]'>
       <div className='flex h-full flex-col items-center justify-center overflow-hidden bg-blackish'>
-        {/* <Link className='w-full' href={currentEventUrl}>
-          <div
-            className={`mb-20 flex h-32 w-full items-center bg-blackish text-whitish transition duration-300 ease-in-out hover:bg-whitish hover:text-blackish sm:h-36 md:h-40 lg:h-64 `}
-          >
-            <h1 className='w-full select-none whitespace-nowrap text-center font-anton text-6xl sm:text-8xl md:text-9xl lg:text-10xl'>
-              OSLO OMVENDT
-            </h1>
-          </div>
-        </Link> */}
         <HeroLink />
       </div>
     </main>
--- src/components/HeroLink.jsx
+++ src/components/HeroLink.jsx
@@ -20,3 +20,15 @@ const HeroLink = () => {
 };
 
 export default HeroLink;
+
+{
+  /* <Link className='w-full' href={currentEventUrl}>
+          <div
+            className={`mb-20 flex h-32 w-full items-center bg-blackish text-whitish transition duration-300 ease-in-out hover:bg-whitish hover:text-blackish sm:h-36 md:h-40 lg:h-64 `}
+          >
+            <h1 className='w-full select-none whitespace-nowrap text-center font-anton text-6xl sm:text-8xl md:text-9xl lg:text-10xl'>
+              OSLO OMVENDT
+            </h1>
+          </div>
+        </Link> */
+}
