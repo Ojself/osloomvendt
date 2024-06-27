dd7e9d4671b224ab0e094a8077847f2f5f267b89
Tormod <tormod.flesjo@gmail.com>
2024-06-27 18:27:35 +0200

    quick commit

--- src/app/week/[week]/page.js
+++ src/app/week/[week]/page.js
@@ -26,9 +26,10 @@ const eventsQuery = groq`*[_type == 'event' && location->region == "oslo" && !(_
 const getData = async (weekNumber) => {
   const currentWeekN = currentWeekNumber();
   const currentYear = new Date().getFullYear();
-
+  const isLessThan3MontsAhead = weekNumber < currentWeekN + 12;
+  const isLessThan3MonthsBehind = weekNumber > currentWeekN - 12;
   const year =
-    weekNumber < currentWeekN + 8 || weekNumber > currentWeekN + 33
+    isLessThan3MontsAhead || isLessThan3MonthsBehind
       ? currentYear
       : currentYear - 1;
   const startDate = getDateOfIsoWeek(weekNumber, year);
