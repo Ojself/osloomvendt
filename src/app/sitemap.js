092c310b778a6fe1818e503c5e0e7522c2a3deea
Tormod <tormod.flesjo@gmail.com>
2024-07-21 20:56:45 +0200

    quick commit

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,6 +2,18 @@ import currentWeekNumber from 'current-week-number';
 export default async function sitemap() {
   const links = [
     {
+      url: 'https://osloomvendt.no/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.69,
+    },
+    {
+      url: 'https://osloomvendt.no/terms-of-sale',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.69,
+    },
+    {
       url: 'https://osloomvendt.no',
       lastModified: new Date(),
       changeFrequency: 'weekly',
@@ -26,37 +38,37 @@ export default async function sitemap() {
       priority: 0.7,
     },
     {
-      url: 'https://www.osloomvendt.no/shop',
+      url: 'https://osloomvendt.no/shop',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.92,
     },
     {
-      url: 'https://www.osloomvendt.no/supporters',
+      url: 'https://osloomvendt.no/supporters',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: 'https://www.osloomvendt.no/packing-list',
+      url: 'https://osloomvendt.no/packing-list',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.89,
     },
     {
-      url: 'https://www.osloomvendt.no/links',
+      url: 'https://osloomvendt.no/links',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://www.osloomvendt.no/blog/musikkfest-2024',
+      url: 'https://osloomvendt.no/blog/musikkfest-2024',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.6,
     },
     {
-      url: 'https://www.osloomvendt.no/blog/norwegian-summer-festival-guide-2024',
+      url: 'https://osloomvendt.no/blog/norwegian-summer-festival-guide-2024',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.65,
@@ -65,19 +77,19 @@ export default async function sitemap() {
 
   const products = [
     {
-      url: 'https://www.osloomvendt.no/shop/oslo-omvendt-t-shirt-black',
+      url: 'https://osloomvendt.no/shop/oslo-omvendt-t-shirt-black',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.85,
     },
     {
-      url: 'https://www.osloomvendt.no/shop/oslo-omvendt-hoodie-black',
+      url: 'https://osloomvendt.no/shop/oslo-omvendt-hoodie-black',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.85,
     },
     {
-      url: 'https://www.osloomvendt.no/shop/oslo-omvendt-choker',
+      url: 'https://osloomvendt.no/shop/oslo-omvendt-choker',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.81,
