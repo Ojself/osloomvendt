f324596e3a9d3acc53fc4c47364c06e7436b1bee
Tormod <tormod.flesjo@gmail.com>
2024-07-21 19:59:27 +0200

    quick commit

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -4,61 +4,61 @@ export default async function sitemap() {
     {
       url: 'https://osloomvendt.no',
       lastModified: new Date(),
-      changefreq: 'weekly',
+      changeFrequency: 'weekly',
       priority: 0.95,
     },
     {
       url: 'https://osloomvendt.no/faq',
       lastModified: new Date(),
-      changefreq: 'monthly',
+      changeFrequency: 'monthly',
       priority: 0.78,
     },
     {
       url: 'https://osloomvendt.no/suggest',
       lastModified: new Date(),
-      changefreq: 'monthly',
+      changeFrequency: 'monthly',
       priority: 0.76,
     },
     {
       url: 'https://osloomvendt.no/2023-wrapped',
       lastModified: new Date(),
-      changefreq: 'monthly',
+      changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
       url: 'https://www.osloomvendt.no/shop',
       lastModified: new Date(),
-      changefreq: 'weekly',
+      changeFrequency: 'weekly',
       priority: 0.92,
     },
     {
       url: 'https://www.osloomvendt.no/supporters',
       lastModified: new Date(),
-      changefreq: 'weekly',
+      changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
       url: 'https://www.osloomvendt.no/packing-list',
       lastModified: new Date(),
-      changefreq: 'monthly',
+      changeFrequency: 'monthly',
       priority: 0.89,
     },
     {
       url: 'https://www.osloomvendt.no/links',
       lastModified: new Date(),
-      changefreq: 'monthly',
+      changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
       url: 'https://www.osloomvendt.no/blog/musikkfest-2024',
       lastModified: new Date(),
-      changefreq: 'monthly',
+      changeFrequency: 'monthly',
       priority: 0.6,
     },
     {
       url: 'https://www.osloomvendt.no/blog/norwegian-summer-festival-guide-2024',
       lastModified: new Date(),
-      changefreq: 'monthly',
+      changeFrequency: 'monthly',
       priority: 0.65,
     },
   ];
@@ -70,8 +70,8 @@ export default async function sitemap() {
     return {
       url: `https://osloomvendt.no/week/${i + 1}`,
       lastModified: new Date(),
-      changefreq: 'monthly',
-      changefreq: isSameAsWeek
+      changeFrequency: 'monthly',
+      changeFrequency: isSameAsWeek
         ? 'daily'
         : isWithinTwoWeeks
           ? 'weekly'
