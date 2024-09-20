b8eeeeb5f78e9ebe0c3e88cc3f3ca3ffaa6fdfd8
Tormod <tormod.flesjo@gmail.com>
2024-09-20 23:49:56 +0200

    quick commit

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -1,4 +1,5 @@
 import NavBar from '@/components/layout/NavBar';
+import './style.css';
 
 export const metadata = {
   metadataBase: new URL('https://www.osloomvendt.no'),
@@ -25,14 +26,7 @@ export default function Layout({ children }) {
   return (
     <>
       <NavBar showLogo={false} />
-
-      <main
-        style={{
-          backgroundImage:
-            'radial-gradient(at 94% 88%, #0F0A19 0px, transparent 50%), radial-gradient(at 27% 73%, #0F0A19 0px, transparent 50%), radial-gradient(at 38% 19%, #0F0A19 0px, transparent 50%), radial-gradient(at 65% 23%, #8B61E5 0px, transparent 50%), radial-gradient(at 7% 90%, #8B61E5 0px, transparent 50%)',
-        }}
-        className='min-h-screen w-full bg-blackish bg-fixed bg-no-repeat px-2 pb-20 font-source-sans text-whitish'
-      >
+      <main className='radial-layout min-h-screen w-full  bg-blackish  bg-cover bg-fixed  bg-center  bg-no-repeat px-2 pb-20 font-source-sans text-whitish '>
         {children}
       </main>
     </>
--- src/app/week/[week]/layout.js
+++ src/app/week/[week]/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
           backgroundImage:
             'radial-gradient(at 94% 88%, #0F0A19 0px, transparent 50%), radial-gradient(at 27% 73%, #0F0A19 0px, transparent 50%), radial-gradient(at 38% 19%, #0F0A19 0px, transparent 50%), radial-gradient(at 65% 23%, #8B61E5 0px, transparent 50%), radial-gradient(at 7% 90%, #8B61E5 0px, transparent 50%)',
         }}
-        className='min-h-screen w-full  bg-fixed  bg-no-repeat px-2 pb-20 pt-32 font-source-sans  text-whitish md:pt-40'
+        className='min-h-screen w-full  bg-cover bg-fixed  bg-no-repeat px-2 pb-20 pt-32 font-source-sans  text-whitish md:pt-40'
       >
         {children}
       </main>
