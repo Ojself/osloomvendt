979de5d5ca9f5594111f92b17cfa3d0809f0675c
Tormod <tormod.flesjo@gmail.com>
2024-11-03 18:55:04 +0100

    quick commit

--- src/components/HeroLink.jsx
+++ src/components/HeroLink.jsx
@@ -13,7 +13,7 @@ const HeroLink = () => {
         className={`mb-10 flex h-32 w-full items-center  text-whitish transition duration-300 ease-in-out hover:bg-whitish hover:text-blackish sm:h-36 md:h-40 lg:h-64 `}
       >
         <h1 className='w-full select-none whitespace-nowrap text-center font-anton text-5xl sm:text-8xl md:text-9xl lg:text-10xl'>
-          🎃SLO 🎃MVENDT
+          OSLO OMVENDT
         </h1>
       </div>
     </Link>
